e94763aaa99eb1922039a1491294975351ce5599
Adam Perlis <adam.perlis@gmail.com>
2018-03-26 20:16:50 -0400

    attempting to style nav bar

--- components/simpleAppBar.js
+++ components/simpleAppBar.js
@@ -1,44 +1,65 @@
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
+import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 import SimpleDrawer from './simpleDrawer';
 import Link from 'next/link'
+import AcademyLogoSmall from '../assets/academy-logo-small.svg'
+import Grid from 'material-ui/Grid';
 
 const styles = {
   root: {
     flexGrow: 1,
+    display:'flex'
   },
+  rightAlign:{
+    flexGrow:1,
+  }
 };
 
 function SimpleAppBar(props) {
   const { classes } = props;
   return (
     <div className={classes.root}>
-      <AppBar position="static" color="default">
+      <AppBar position="sticky" color="default">
         <Toolbar>
-          <Link href="/">
+
+        <Grid item xs lg={4}>
+          <IconButton href="#flat-buttons" className={classes.button}>
+            <Link href="/">
+              <AcademyLogoSmall />
+            </Link>
+          </IconButton>
+        </Grid>
+
+          <Grid item xs lg={8} className={classes.rightAlign}>
+          <Button href="/work" className={classes.button}>
             <Typography variant="title" color="inherit">
-            Title
+              Work
             </Typography>
-          </Link>
+          </Button>
 
-          <Link href="/work">
-            <a>Our Work</a>
-          </Link>
+          <Button href="/process" className={classes.button}>
+            <Typography variant="title" color="inherit">
+              Our Process
+            </Typography>
+          </Button>
 
-          <Link href="/process">
-            <a>Our Process</a>
-          </Link>
+          <Button href="/workshops" className={classes.button}>
+            <Typography variant="title" color="inherit">
+              Workshops
+            </Typography>
+          </Button>
 
-          <Link href="/workshops">
-            <a>Workshops</a>
-          </Link>
-
-          <Link href="/blog">
-            <a>Blog</a>
-          </Link>
+          <Button href="/blog" className={classes.button}>
+            <Typography variant="title" color="inherit">
+              Blog
+            </Typography>
+          </Button>
+          </Grid>
 
           <SimpleDrawer />
         </Toolbar>
