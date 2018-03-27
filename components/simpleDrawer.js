cd8487a7a380142583133e656164ff8f1b1e4f78
Adam Perlis <adam.perlis@gmail.com>
2018-03-27 18:06:49 -0400

    started adding more stuff to the drawer

--- components/simpleDrawer.js
+++ components/simpleDrawer.js
@@ -5,12 +5,34 @@ import Button from 'material-ui/Button';
 import Divider from 'material-ui/Divider';
 import Icon from 'material-ui/Icon';
 import Link from 'next/link'
+import Typography from 'material-ui/Typography';
 
 const styles = {
 
 };
 
+const transition1 = {
+  background: 'lightgray',
+  width: '100%',
+  height: '100vh',
+  position: 'relative',
+  zIndex: -1,
+  display:'block',
+  transition: '1s all'
+};
+
+const transition2 = {
+  background: 'gray',
+  width: '100%',
+  height: '100vh',
+  position: 'relative',
+  zIndex: -1,
+  display:'block',
+  transition: '1s all'
+};
+
 class SimpleDrawer extends React.Component {
+
   state = {
     top: false
   };
@@ -34,12 +56,21 @@ class SimpleDrawer extends React.Component {
             onClick={this.toggleDrawer('top', false)}
             onKeyDown={this.toggleDrawer('top', false)}
           >
+
           <Link href="/">
-            <a>Home</a>
+            <Typography variant="display2" color="inherit">
+              Work
+            </Typography>
           </Link>
           <Link href="/about">
-            <a>About</a>
+            <Typography variant="display2" color="inherit">
+              About
+            </Typography>
           </Link>
+
+          <div style={transition2}></div>
+          <div style={transition1}></div>
+
           </div>
         </Drawer>
       </div>
