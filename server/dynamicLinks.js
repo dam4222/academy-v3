5d76c370bc3ff5ffcf84f48756dcc9897020dfff
Omkar <ork216@nyu.edu>
2018-06-11 00:43:22 -0400

    added support for sitemap.xml and robots.txt

---
+++ server/dynamicLinks.js
@@ -0,0 +1,49 @@
+const fetchUrl = process.env.adminUrl;
+const baseUrl = "https://" + fetchUrl + "/wp-json/wp/v2/";
+require('isomorphic-fetch');
+
+const fetchData = async (url) => {
+    try{
+        const dataRes = await fetch(url);
+        const dataJson = await dataRes.json();
+        if (dataJson !== undefined)
+            return dataJson;
+        else{
+            return {"code": "error"}
+        }   
+    }
+    catch(error){
+        console.error(error);
+        return  {"code": "error"}
+    }
+}
+
+//fetching blogPosts' slugs for sitemap
+const data = async (postType) => {
+    let Posts = [];
+    
+    const url = baseUrl + postType + "?per_page=100&page=";
+
+    //while we receive Postyle: blogPosts or Projects, receive 'em and append results
+    let pageNo = 1;
+    while (true){
+        let Json = await fetchData(url + pageNo);
+        if ("code" in Json)
+            break
+        
+        Json.map((type) => {
+            Posts.push({ slug: `${type.slug}` })
+        })
+        pageNo = pageNo + 1;
+    }
+    return Posts;
+}
+    
+
+//fetching blogPosts' slugs for sitemap
+const blogs = async () => await data("blogs")
+
+//fetching Projects' slugs for sitemap
+const projects = async () => await data("projects") 
+
+module.exports =  { blogs, projects }
---
+++ server/sitemapAndRobots.js
@@ -0,0 +1,97 @@
+const sm = require('sitemap')
+const path = require('path')
+const { blogs, projects } = require('./dynamicLinks');
+
+let sitemap;
+
+const createMap = async () => {
+
+  //fetching blogs from dynamicLink.js and appending it to the sitemap
+  sitemap = sm.createSitemap({
+    hostname: 'https://academyux.com',
+    cacheTime: 600000 // 600 sec - cache purge period
+  });
+
+  await blogs().then((posts) => {
+    posts.map((post) => {
+      sitemap.add({
+        url: `/post?${post.slug}`,
+        changefreq: 'daily',
+        priority: 0.9
+      })
+    })
+  })
+  .catch(err => console.error(err))
+    
+  //fetching projects from dynamicLink.js and appending it to the sitemap
+  await projects().then((posts) => {
+    posts.map((project) => {
+      sitemap.add({
+        url: `/project?${project.slug}`,
+        changefreq: 'daily',
+        priority: 0.9
+      })
+    })
+  })
+  .catch(err => console.error(err))
+
+  sitemap.add({
+    url: '/process',
+    changefreq: 'daily',
+    priority: 1
+  })
+
+  sitemap.add({
+    url: '/workshops',
+    changefreq: 'daily',
+    priority: 1
+  })
+
+  sitemap.add({
+    url: '/work',
+    changefreq: 'daily',
+    priority: 1
+  })
+
+  sitemap.add({
+    url: '/blog',
+    changefreq: 'daily',
+    priority: 1
+  })
+
+  sitemap.add({
+    url: '/about',
+    changefreq: 'daily',
+    priority: 1
+  })
+
+  sitemap.add({
+    url: '/contact',
+    changefreq: 'daily',
+    priority: 1
+  })
+}
+
+
+const setup = async ({ server }) => {
+
+  server.get('/sitemap.xml', async (req, res) => {
+    //creating sitemap for everyrequest
+    await createMap();
+    await sitemap.toXML((err, xml) => {
+      if (err) {
+        res.status(500).end()
+        return
+      }
+
+      res.header('Content-Type', 'application/xml')
+      res.send(xml)
+    })
+  })
+
+  server.get('/robots.txt', (req, res) => {
+    res.sendFile(path.join(__dirname, '../static', 'robots.txt'))
+  })
+}
+
+module.exports = setup
