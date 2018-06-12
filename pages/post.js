19c709c044c0b341afcb897b4b028bcd02087ea2
Adam Perlis <adam.perlis@gmail.com>
2018-06-11 20:18:22 -0400

    fixing authors again

--- pages/post.js
+++ pages/post.js
@@ -125,20 +125,22 @@ class Post extends React.Component {
 
             <Grid item xs={12} style={{paddingBottom:'40px'}}>
               <Grid container>
-                <Grid item xs={12} md={6} style={{display:'flex', alignItems:'center'}}>
+
               {this.props.authors.map((author) => {
                 return(
-                  <div style={{paddingRight:'20px', alignItems: 'center', display: 'flex'}}>
+                  <Grid item xs={6} sm={4} md={4} lg={5} xl={3}>
+                  <div style={{alignItems: 'center', display: 'flex'}}>
                   <div className="author-profile" dangerouslySetInnerHTML={{__html: author.user_avatar}}></div>
                   <div style={{display: 'inline-block', paddingLeft: '15px'}}>
                   <Typography variant="button">{author.display_name}</Typography>
                   <Typography variant="caption"><span dangerouslySetInnerHTML={{__html:author.user_description}}></span></Typography>
                   </div>
                   </div>
+                  </Grid>
                 )
 
               })}
-                </Grid>
+
               </Grid>
 
             </Grid>
