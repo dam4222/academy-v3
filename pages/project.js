a469a88b9ade89c4c91403d18103e296470406cb
orkeluskar <ork216@nyu.edu>
2018-08-07 15:13:57 -0400

    Fixed dialog box not appearing

--- pages/project.js
+++ pages/project.js
@@ -17,12 +17,12 @@ import Error from 'next/error';
 import SimpleForm from '../components/simpleForm';
 
 
-import Dialog, {
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-} from '@material-ui/core/Dialog'
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
 
 import 'isomorphic-fetch'
 const fetchUrl = process.env.fetchUrl;
